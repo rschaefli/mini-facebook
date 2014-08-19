4ab13d6cef8c91b2825ed37ec29e1c28ad088d38
Ricardo Schaefli <rix@Ricardos-MacBook-Pro.local>
2014-08-19 19:36:40 -0400

    Putting together friendship relationships, adding a friend still WIP.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  # define relationships with a Friendship here
   has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 end
